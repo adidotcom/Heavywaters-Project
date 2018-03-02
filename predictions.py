e753ccb43c84f1cd03e32efff1ba1e1aa06a76d6
Aditya Kamath <36966986+adidotcom@users.noreply.github.com>
2018-03-02 03:39:32 -0500

    Update predictions.py

--- predictions.py
+++ predictions.py
@@ -1,3 +1,4 @@
+# Importing necessary modules
 from sklearn.externals import joblib
 from boto.s3.key import Key
 from boto.s3.connection import S3Connection
@@ -11,7 +12,7 @@ MODEL_LOCAL_PATH = '/tmp/' + MODEL_FILE_NAME
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['POST'])
 def index():
   payload = json.loads(request.get_data().decode('utf-8'))
   prediction = predict(payload['payload'])
@@ -29,6 +30,5 @@ def load_model():
   return joblib.load(MODEL_LOCAL_PATH)
 
 def predict(data):
-  # Process your data, create a dataframe/vector and make your predictions
-  #final_formatted_data = data['data']
+  # Data to be processed here and sent for prediction
   return load_model().predict(data)
